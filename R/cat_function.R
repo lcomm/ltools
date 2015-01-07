29575623867e7ca33f4d65ff19ef440f3c25c692
lcomm <leah.comment@gmail.com>
2015-01-07 13:57:22 -0500

    added description, namespace, and first function (cat_function)

---
+++ R/cat_function.R
@@ -0,0 +1,20 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' This is Hilary Parker's example function.
+#' For some reason, my projects don't really use it too often.
+#' Perhaps I am studying the wrong kind of statistics.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+#' 
+cat_function <- function(love=TRUE){
+    if(love==TRUE){
+        print("I love cats!")
+    }
+    else {
+        print("I am not a cool person.")
+    }
+}
