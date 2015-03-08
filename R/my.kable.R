a065eb21dc09eeb7b8b1b2e8d152854126110928
lcomm <leah.comment@gmail.com>
2015-03-07 21:16:58 -0500

    dramatically simplified (basically rewritten) in response to changes to base kable

--- R/my.kable.R
+++ R/my.kable.R
@@ -1,85 +1,35 @@
 #' Customized kable (table-making function for knitr)
 #' 
 #' Function to adapt kable function from knitr package to meet most of my
-#' table-making needs. Tries to do a lot of things, some of which it does
-#' successfully. Right-aligns numeric columns and left-aligns character
-#' columns, pretties up numbers using prettyNum (if desired), escapes out
-#' LaTeX characters (if desired). Writes the table in LaTeX code assuming
-#' that the booktabs package is being used. Definitely a work in progress.
-#' @param my.kable.obj Object (matrix or data frame) passed in to be kabled
-#' @param caption Caption for the table. Defaults to no caption.
-#' @param digits Number of significant digits for numbers in the table. This 
-#' sometimes works. Defaults to 4.
-#' @param latexTranslate Whether or not the latexTranslate function from the 
-#' Hmisc package should be used to escape out LaTeX-unfriendly characters.  
-#' This tends not to work well for complicated cases. Defaults to FALSE.
-#' @param greek Argument to latexTranslate function. Again, this only works 
-#' sometimes. Defaults to TRUE.
-#' @param prettyNum Whether prettyNum should be used to align decimals, etc.
-#' Occasionally problematic. Defaults to TRUE.
+#' table-making needs. This intentionally removes the table environment, so the
+#' tables do not float. Unfortunately, that means it's hit or miss on whether 
+#' the caption stays near the table.  This function is ugly 
+#' @param kable.obj Object (matrix or data frame) passed in to be kabled
+#' @param booktabs Whether booktabs style should be used.  Defaults to TRUE.
+#' @param format Type of table to be made.  Defaults to "latex".
+#' @param escape Whether special LaTeX characters should be escaped.  
+#' The default in kable right now is to escape these characters, but for some 
+#' reason, escape never works quite like I want it to. Defaults to FALSE.
+#' @param caption Caption for the table. Defaults to NULL (no caption).
+#' @param ... Other parameters passed to kable
 #' @keywords knitr tables
-#' @seealso kable, latexTranslate
+#' @seealso kable
 #' @export
-#' @examples
-#' ## Fake table, an example where greek=TRUE actually works out
-#' my.table = data.frame(ABCs = LETTERS[1:3], beta=1:3, values=10^(-1:-3))
-#' my.kable(my.table, latexTranslate=TRUE, greek=TRUE)
 #' 
-my.kable <- function(my.kable.obj, caption=NULL, digits=4, latexTranslate=FALSE
-                    , greek=TRUE, prettyNum=TRUE){
-    #Need these
-    library(knitr)
-    library(Hmisc)
-    #Figure out which things are numeric (need to right-align)
-    aligns = rep("l",ncol(my.kable.obj))
-    my.kable.obj = as.data.frame(my.kable.obj)
-    numcols = which(as.logical(sapply(my.kable.obj,is.numeric)))
-    aligns[numcols] = "r"
-    alignment = paste(aligns,collapse="")
-    alignment = paste0("{",alignment,"}")
-    pastething2 = paste0("\\begin{table*}[h]\\centering\\begin{tabular}"
-                         ,alignment)
-    #Format numbers
-    if (prettyNum == TRUE) {
-        my.kable.obj = prettyNum(my.kable.obj, digits=digits)
-    } else {
-        my.kable.obj[,numcols] = formatC(my.kable.obj[,numcols], digits=digits)
+my.kable = function(kable.object, booktabs=TRUE, escape=FALSE, format="latex", caption=NULL, ...){
+    #Handle caption if necessary
+    if (!is.null(caption)) { 
+        paste.capt = paste0("\\captionof{table}{",caption,"}")
+    } else{
+        paste.capt = ""
     }
-    #Make the caption, if applicable
-    if (length(caption) > 0){
-        pastething2 = paste0("\\begin{table*}[h]\\caption*{",caption
-                             ,"}\\centering\\begin{tabular}",alignment)
-    } else {
-        pastething2 = paste0("\\begin{table*}[h]\\centering\\begin{tabular}"
-                             ,alignment)
-    }
-    #Write the latex
-    if (latexTranslate==TRUE){
-        gsub("\\&", "&"
-             ,gsub("\\end{tabular}", "\\end{tabular}\\end{table*}"
-                   ,gsub("\\begin{tabular}", pastething2
-                         ,gsub("\\addlinespace", ""
-                               ,gsub(pattern = "\\{[lrc]*\\}", replacement=""
-                                     ,latexTranslate(kable(my.kable.obj
-                                                           ,format="latex"
-                                                           ,booktabs=TRUE)
-                                                     ,greek=greek))
-                               ,fixed=TRUE)
-                         ,fixed=TRUE)
-                   ,fixed=TRUE)
-             ,fixed=TRUE)
-    } else {
-        gsub("\\&", "&"
-             ,gsub("\\end{tabular}", "\\end{tabular}\\end{table*}"
-                   ,gsub("\\begin{tabular}", pastething2
-                         ,gsub("\\addlinespace", ""
-                               ,gsub(pattern = "\\{[lrc]*\\}", replacement=""
-                                     ,kable(my.kable.obj,format="latex"
-                                            ,booktabs=TRUE))
-                               ,fixed=TRUE)
-                         ,fixed=TRUE)
-                   ,fixed=TRUE)
-             ,fixed=TRUE)
-    }
+    starting = paste0("\\begin{center}", paste.capt, "\\begin{tabular}")
+    
+    #Write table output
+    gsub("\\begin{tabular}", starting
+         ,gsub("\\end{tabular}", "\\end{tabular}\\end{center}"
+               ,gsub("\\addlinespace", ""
+                     ,gsub("NA", ""
+                           ,kable(kable.object, format="latex", booktabs=TRUE, escape=FALSE)
+                     ), fixed=TRUE), fixed=TRUE), fixed=TRUE)
 }
-
