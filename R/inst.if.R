4166bd6ab4dcfa6de2e4950ec936d218bf5eda00
lcomm <leah.comment@gmail.com>
2015-07-27 20:19:30 -0400

    initial commit for inst.if function

---
+++ R/inst.if.R
@@ -0,0 +1,15 @@
+#' Function to install and load packages if necessary
+#' 
+#' This function checks to see if a package is installed yet
+#' If not, it installs it.  Either way, once installed, it loads the library.
+#' @param pkgname The name of the desired package as a string
+#' @examples
+#' inst.if("Matrix")
+inst.if = function(pkgname){
+    #Install if not in the list of available packages
+    if (!require(pkgname, character.only=TRUE)) { 
+        install.packages(eval(pkgname)) 
+    }
+    #Load
+    library(pkgname, character.only=TRUE)
+}
