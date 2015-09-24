2b7af16ff13634f6df65a4beabe28dc1cf19a9cd
lcomm <leah.comment@gmail.com>
2015-09-24 16:54:54 -0400

    Add fraction support

--- R/latexMat.R
+++ R/latexMat.R
@@ -2,18 +2,28 @@
 #' 
 #' @param mat The matrix to be printed
 #' @param matType Bracketing type for the matrix. Defaults to "pmatrix"
+#' @param fractions Should we write the elements in fraction form (uses MASS package)? Defaults to TRUE
 #' 
+#' @seealso fractions
 #' @export
 #' @examples
 #' ## Make the matrix
-#' a <- rbind(1:4,5:8)
+#' a <- rbind(1:4, 5:8/10)
 #' 
 #' ## Apply the print function
 #' latexMat(a)
 #' 
-latexMat <- function(mat, matType="pmatrix"){
+latexMat <- function(mat, matType = "pmatrix", fractions = TRUE){
     #Insert & to separate elements in a row
-    rows <- apply(mat,1, FUN=function(row) { paste(row,collapse=" & ") })
+    rows <- apply(mat, 1, FUN = function(row) 
+        { 
+            if (fractions) {
+                require("MASS")
+                paste(fractions(row), collapse=" & ") 
+            } else {
+                paste(row, collapse=" & ")     
+            }
+        })
     
     #Add line breaks between rows
     wholemat <- paste(rows, collapse=" \\\\ ")
