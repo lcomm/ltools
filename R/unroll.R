3c6f982c11fa957f3e7eff20942cb108a05a5b0e
lcomm <leah.comment@gmail.com>
2016-05-22 14:32:01 -0400

    Initial commit

---
+++ R/unroll.R
@@ -0,0 +1,11 @@
+#' Unroll a list and save its elements into the global environment
+#' 
+#' Used for its side effects
+#' 
+#' @param list Name of the list to unroll
+#' 
+unroll <- function(list){
+    for (i in 1:length(list)){
+        assign(names(list)[i], list[[i]], envir=.GlobalEnv)
+    }
+}
