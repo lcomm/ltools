20e9f8a6eefa60cec837a2d1ec3cffc07d1a8664
lcomm <leah.comment@gmail.com>
2016-07-26 17:04:35 -0400

    Changed to easier plogis (maybe not as numerically stable, but faster)

--- R/expit.R
+++ R/expit.R
@@ -4,6 +4,7 @@
 #' expit(x) = e^x/(1 + e^x) = 1/(1 + e^(-x))
 #' More numerically stable if you calculate the function
 #' differently based on whether x is positive or negative
+#' Note: changing to simpler (faster) evaluation using plogis
 #' @param input Argument to the inverse logit function
 #' @keywords inverse-logit
 #' @export
@@ -13,14 +14,4 @@
 #' ## Matrix of values
 #' expit(matrix(1:4,2,2))
 #' 
-expit <- function(x){
-    ifelse(x >= 0, 1/(1+exp(-x)), function(x) {z=exp(x); z/(1+z)})
-}
-
-expit2 <- function(x){
-    ifelse(x >=0, 1/(1+exp(-x)),, exp(x)/(1+exp(x)))
-}
-
-expit3 <- function(){
-    1/(1+exp(-x))
-}
+expit <- plogis
